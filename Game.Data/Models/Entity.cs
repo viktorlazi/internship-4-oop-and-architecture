d7976f385db6ff1d12eed3a6447c92e101c302e2
Viktorlazi <viktorlazi010@gmail.com>
2020-12-07 15:21:38 +0100

    entity class

---
+++ Game.Data/Models/Entity.cs
@@ -0,0 +1,13 @@
+
+namespace Game.Data.Models
+{
+    public class Entity
+    {      
+        public string Name {get;set;}
+        public int Hp {get;set;}
+        public int Damage {get;set;}
+        public void Attack(){
+
+        }
+    }
+}
