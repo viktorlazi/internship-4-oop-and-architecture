636cb3e338f8c5728b2a0adb170fe6a246fec3bd
Viktorlazi <viktorlazi010@gmail.com>
2020-12-12 19:37:49 +0100

    You killed yourself in a charge message

--- Game.Domain/GameCycle/Fight.cs
+++ Game.Domain/GameCycle/Fight.cs
@@ -113,6 +113,9 @@ namespace Game.Domain.GameCycle{
                 if(Console.ReadLine() == "charge"){
                     var dmg = ((Warrior)p).Charge(enemy);
                     DisplayText.ColorLine("You charge your enemy with " + dmg + " damage at the cost of 10% HP", ConsoleColor.Yellow);
+                    if(p.Hp <= 0){
+                        System.Console.WriteLine("Aaaand... you killed yourself in a charge");
+                    }
                     DungeonData.Log(ConsoleColor.Green, "You charge " + enemy.ToString() + " for " + dmg + " and lose 10% hp \t\t" + p.Hp + " | " + enemy.Hp);
                 }else{
                     var dmg = p.Hit(enemy);
